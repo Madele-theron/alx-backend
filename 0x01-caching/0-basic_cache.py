1458c60eb25e2750240d5722ff7bfea2be435a93
Madele <madele.theron@gmail.com>
2023-05-16 22:13:33 +0200

    task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+"""A module that """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Inherits from BaseCaching and is a caching system"""
+    def put(self, key, item):
+        """
+        Add item to cache
+
+        Args:
+            key: to identify the item
+            item: item to be cached
+
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve item from cache
+
+        Args:
+            key: associated with the item.
+
+        Returns:
+            Item value if found in the cache, else None.
+
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
